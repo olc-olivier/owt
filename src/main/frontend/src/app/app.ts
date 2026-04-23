6e52775d88755e38aa15e34ac660e063ee419ccf
Olivier Chantereau <olivier.chantereau@gmail.com>
2026-04-23 10:36:32 +0200

    Add Material Design components for Angular

--- src/main/frontend/src/app/app.ts
+++ src/main/frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
